e278cc102927dcced28c537c5f5a4a1290fde6d3
nickWoott <nicholaswootton@hotmail.co.uk>
2024-09-09 18:42:52 +0100

    [RED] add failing test to return empty PricingRules struct

--- pricing/pricing_test.go
+++ pricing/pricing_test.go
@@ -1,1 +1,17 @@
 package pricing_test
+
+import (
+	"testing"
+
+	"github.com/nickWoott/go-supermarket-checkout-kata/pricing"
+)
+
+func TestPricing(t *testing.T) {
+	t.Run("return empty PricingRules struct", func(t *testing.T) {
+		pr := pricing.NewPricingRules()
+
+		if pr == nil {
+			t.Fatalf("Expected PricingRules struct, got nil")
+		}
+	})
+}
