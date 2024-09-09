a4f05c11a0d83c69b76358aebf4315fdd7289072
nickWoott <nicholaswootton@hotmail.co.uk>
2024-09-09 19:10:41 +0100

    [RED] add failing test for expected pricing rules

--- pricing/pricing_test.go
+++ pricing/pricing_test.go
@@ -7,11 +7,37 @@ import (
 )
 
 func TestPricing(t *testing.T) {
-	t.Run("return empty PricingRules struct", func(t *testing.T) {
+	t.Run("returns PricingRules map", func(t *testing.T) {
 		pr := pricing.NewPricingRules()
 
 		if pr == nil {
-			t.Fatalf("Expected PricingRules struct, got nil")
+			t.Errorf("Expected PricingRules map, got nil")
+		}
+	})
+
+	t.Run("returns expected pricing rules", func(t *testing.T) {
+		pr := pricing.NewPricingRules()
+
+		expectedRules := PricingRules{
+			"A": {UnitPrice: 50, SpecialPriceQuantity: 3, SpecialPriceAmount: 130},
+			"B": {UnitPrice: 30, SpecialPriceQuantity: 2, SpecialPriceAmount: 45},
+			"C": {UnitPrice: 20},
+			"D": {UnitPrice: 15},
+		}
+
+		if len(pr) != len(expectedRules) {
+			t.Errorf("Expected length %d, but got %d", len(expectedRules), len(pr))
+		}
+
+		for sku, expectedRule := range expectedRules {
+			actualRule, exists := pr[sku]
+			if !exists {
+				t.Errorf("SKU %s not found in pricing rules", sku)
+				continue
+			}
+			if actualRule != expectedRule {
+				t.Errorf("For SKU %s, expected %+v but got %+v", sku, expectedRule, actualRule)
+			}
 		}
 	})
 }
