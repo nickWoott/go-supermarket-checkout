e06cd6588859c1f23e59a1ba9ac171f697bc8655
nickWoott <nicholaswootton@hotmail.co.uk>
2024-09-09 18:17:19 +0100

    [RED] add failing test for return empty checkout struct

--- checkout/checkout_test.go
+++ checkout/checkout_test.go
@@ -1,1 +1,18 @@
 package checkout_test
+
+import (
+	"testing"
+
+	"github.com/nickWoott/go-supermarket-checkout-kata/checkout"
+)
+
+func TestCheckout(t *testing.T) {
+	t.Run("return empty Checkout struct", func(t *testing.T) {
+		co := checkout.NewCheckout()
+
+		if co == nil {
+			t.Fatalf("Expected a Checkout struct, but got nil")
+		}
+
+	})
+}
